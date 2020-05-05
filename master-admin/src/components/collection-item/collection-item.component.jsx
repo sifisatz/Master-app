7624e913ea9a2fb6579ad2d6b7f9053f4ec090ee
sifisatz <iwsifatz@gmail.com>
2020-05-05 16:53:06 +0300

    materials and collection item, preview

---
+++ master-admin/src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './colection-item.styles.scss'
+
+export default function CollectionItem({ id, name, price, imageUrl }) {
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            >
+                <div className="collection-footer">
+                    <span className="name">{name} </span>
+                    <span className="price">{price} </span>
+                </div>
+            </div>
+
+        </div>
+    )
+}
